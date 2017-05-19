1387c9a04b2f197f2d2ed993e6a38fdf62f3f9b2
Fatos Morina <fatosi.morina@gmail.com>
2017-05-19 06:50:15 +0200

    Create Room repository

---
+++ src/main/java/com/fatosmorina/reservations/entity/Room.java
@@ -0,0 +1,46 @@
+package com.fatosmorina.reservations.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ROOM")
+public class Room {
+    private long id;
+    private String name;
+    private String number;
+    private String bedInfo;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getBedInfo() {
+        return bedInfo;
+    }
+
+    public void setBedInfo(String bedInfo) {
+        this.bedInfo = bedInfo;
+    }
+
+}
