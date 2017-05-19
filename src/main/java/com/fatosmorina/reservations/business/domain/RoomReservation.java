3c20a068c648eadc51b23b5787942d1bf7ce1af8
Fatos Morina <fatosi.morina@gmail.com>
2017-05-19 07:32:09 +0200

    Add ReservationController

--- src/main/java/com/fatosmorina/reservations/business/domain/RoomReservation.java
+++ src/main/java/com/fatosmorina/reservations/business/domain/RoomReservation.java
@@ -1,17 +1,70 @@
 package com.fatosmorina.reservations.business.domain;
 
-import static java.lang.Integer.parseInt;
-import static java.lang.System.exit;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.util.StringTokenizer;
-
+import java.util.Date;
 
 public class RoomReservation {
     private long roomId;
     private long guestId;
-    private
+    private String roomName;
+    private String roomNumber;
+    private String firstName;
+    private String lastName;
+    private Date date;
+
+    public long getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(long roomId) {
+        this.roomId = roomId;
+    }
+
+    public long getGuestId() {
+        return guestId;
+    }
+
+    public void setGuestId(long guestId) {
+        this.guestId = guestId;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(String roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
 }
