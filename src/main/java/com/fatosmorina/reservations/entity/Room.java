6e607bd4ad78b42fc9a24a9f3675f338012627b2
Fatos Morina <fatosi.morina@gmail.com>
2017-05-19 06:51:36 +0200

    Add ID annotations at Room repository

--- src/main/java/com/fatosmorina/reservations/entity/Room.java
+++ src/main/java/com/fatosmorina/reservations/entity/Room.java
@@ -1,11 +1,18 @@
 package com.fatosmorina.reservations.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "ROOM")
 public class Room {
+    @Id
+    @Column(name = "ROOM_ID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String name;
     private String number;
