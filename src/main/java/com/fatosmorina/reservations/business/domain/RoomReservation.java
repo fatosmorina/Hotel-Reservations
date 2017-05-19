19222a0a607d64301b748142b0f353e0c32dc1c8
Fatos Morina <fatosi.morina@gmail.com>
2017-05-19 07:12:09 +0200

    Add RoomRepository

---
+++ src/main/java/com/fatosmorina/reservations/business/domain/RoomReservation.java
@@ -0,0 +1,17 @@
+package com.fatosmorina.reservations.business.domain;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.System.exit;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+
+public class RoomReservation {
+    private long roomId;
+    private long guestId;
+    private
+}
