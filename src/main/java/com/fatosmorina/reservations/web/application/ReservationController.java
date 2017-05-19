c29cd5750c55823cc6329c96ab483e3388b100ab
Fatos Morina <fatosi.morina@gmail.com>
2017-05-19 12:19:05 +0200

    Refactor ReservationController

--- src/main/java/com/fatosmorina/reservations/web/application/ReservationController.java
+++ src/main/java/com/fatosmorina/reservations/web/application/ReservationController.java
@@ -1,9 +1,5 @@
 package com.fatosmorina.reservations.web.application;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,24 +15,12 @@ import com.fatosmorina.reservations.business.service.ReservationService;
 @Controller
 @RequestMapping(value = "/reservations")
 public class ReservationController {
-    private static final DateFormat DATE_FORMAT = new SimpleDateFormat("dd-MM-yyyy");
-
     @Autowired
     private ReservationService reservationService;
 
     @RequestMapping(method = RequestMethod.GET)
     public String getReservations(@RequestParam(value = "date", required = false) String dateString, Model model) {
-        Date date = null;
-        if (dateString != null && !"".equals(dateString)) {
-            try {
-                date = DATE_FORMAT.parse(dateString);
-            } catch (ParseException pe) {
-                date = new Date();
-            }
-        } else {
-            date = new Date();
-        }
-        List<RoomReservation> roomReservationList = this.reservationService.getRoomReservationsForDate(date);
+        List<RoomReservation> roomReservationList = this.reservationService.getRoomReservationsForDate(dateString);
         model.addAttribute("roomReservations", roomReservationList);
         return "reservations";
     }
